4053ab1e4f876d866f90917cd7c09919a81b2495
LordZeds <contatotiberioz@gmail.com>
2025-11-26 16:22:09 -0300

    Fix: Middleware ignorando rota da API de ingestao

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,8 +12,9 @@ export const config = {
          * - _next/static (static files)
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
-         * Feel free to modify this pattern to include more paths.
+         * - api/ingest (API Pública de Ingestão - ADICIONADO AQUI)
+         * - images (svg, png, etc)
          */
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|api/ingest|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
